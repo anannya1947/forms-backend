24c6c4297283babfd25e3a0f0f5ce8ff958c9dea
Anannya Mahapatra <anannya1947@gmail.com>
2022-01-03 22:43:57 +0530

    db connection

--- server/index.js
+++ server/index.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const app = express()
+const mysql = require('mysql')
+
+const db = mysql.createPool({
+    hostname: "localhost",
+    user: "root",
+    password: "password",
+    database: "sys",
+})
 
 app.get('/', (req,res) => {
-    res.send("hello, world")
+    const sqlInsert = "INSERT INTO movie_reviews (movieName, movieReview) VALUES ('inception','good movie');"
+    db.query(sqlInsert, (err,result)=> {
+        res.send("hello, world")
+    })
 })
+
 app.listen(3005, () => {
     console.log("running on port 3005")
 })

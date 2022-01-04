cb29c1953fb147b480fc5c6f9bcf07f5af7171c2
Anannya Mahapatra <anannya1947@gmail.com>
2022-01-04 19:42:05 +0530

    successful backend connection

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-Parser')
 const mysql = require('mysql')
+const cors = require('cors')
 
 const db = mysql.createPool({
     hostname: "localhost",
@@ -9,10 +11,18 @@ const db = mysql.createPool({
     database: "sys",
 })
 
-app.get('/', (req,res) => {
-    const sqlInsert = "INSERT INTO movie_reviews (movieName, movieReview) VALUES ('inception','good movie');"
-    db.query(sqlInsert, (err,result)=> {
-        res.send("hello, world")
+app.use(cors())
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.post('/api/insert', (req,res)=>{
+
+    const movieName = req.body.movieName
+    const movieReview = req.body.movieReview
+
+    const sqlInsert="INSERT INTO movie_reviews(movieName, movieReview) VALUES (?,?);"
+    db.query(sqlInsert, [movieName, movieReview], (err,result)=>{
+    console.log(err)
     })
 })
 
